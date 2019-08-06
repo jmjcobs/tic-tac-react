8cbfb18649c3fc7974c2536da1fbdaa14d351364
jmjcobs <jonathanmsj@hotmail.com>
2019-08-06 09:42:36 -0500

    Add ability to view history of moves

--- src/index.js
+++ src/index.js
@@ -55,12 +55,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
   handleClick(squareIndex) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -82,14 +83,37 @@ class Game extends React.Component {
           squares: squares
         }
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    /*  Remember .map takes (value, index, array) as parameters 
+        step is the square values in the current iteration 
+        move is the index of the current move 
+      */
+    const moves = history.map((step, move) => {
+      console.log("history", history, "step", step, "move", move);
+
+      const desc = move ? `Go to Move # ${move}` : `Go to game start`;
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -110,7 +134,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
